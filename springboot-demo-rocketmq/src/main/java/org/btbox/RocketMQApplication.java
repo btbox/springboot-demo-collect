28bb71d355d66d6358fd65f706ab56ec178f31cb
BT-BOX <852649348@qq.com>
2024-07-16 15:55:04 +0800

    feat: springboot-rocketmq

---
+++ springboot-demo-rocketmq/src/main/java/org/btbox/RocketMQApplication.java
@@ -0,0 +1,17 @@
+package org.btbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:
+ * @author: BT-BOX
+ * @createDate: 2024/7/12 上午11:43
+ * @version: 1.0
+ */
+@SpringBootApplication
+public class RocketMQApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RocketMQApplication.class, args);
+    }
+}
